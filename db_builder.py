c7b10051455796995e2f04e69c7a7501b0577168
David Doktorman <ddoktorman@stuy.edu>
2018-01-23 20:51:21 -0800

    added img_name table

--- db_builder.py
+++ db_builder.py
@@ -11,7 +11,7 @@ db = sqlite3.connect(f, check_same_thread=False) #open if f exists, otherwise cr
 db.create_function('encrypt', 1, encrypt_password)
 c = db.cursor()    #facilitate db ops
 
-create_users = "CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT NOT NULL, name TEXT NOT NULL, age INTEGER NOT NULL, gender TEXT NOT NULL, prefGender TEXT NOT NULL, lang TEXT NOT NULL, sortAlg TEXT NOT NULL, type TEXT NOT NULL, bitcoin TEXT NOT NULL, nameCase TEXT NOT NULL, braces TEXT NOT NULL, bio TEXT NOT NULL);"
+create_users = "CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT NOT NULL, name TEXT NOT NULL, age INTEGER NOT NULL, gender TEXT NOT NULL, prefGender TEXT NOT NULL, lang TEXT NOT NULL, sortAlg TEXT NOT NULL, type TEXT NOT NULL, bitcoin TEXT NOT NULL, nameCase TEXT NOT NULL, braces TEXT NOT NULL, bio TEXT NOT NULL, img_name TEXT NOT NULL);"
 
 create_personality = "CREATE TABLE personality (username TEXT PRIMARY KEY, open DECIMAL NOT NULL, consc DECIMAL NOT NULL, extra DECIMAL NOT NULL, agree DECIMAL NOT NULL, emotRange DECIMAL NOT NULL);"
 
